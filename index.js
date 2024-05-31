01ff5290ac90832eafa28916baa892a95667c753
SiddikArim <arim99999@gmail.com>
2024-05-31 12:19:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("news is running");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.listen(port, () => {
+  console.log(`news is running ${port}`);
+});
