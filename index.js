5d8efe5b41f13ba348c9fc15a6bb37ee629b9096
SiddikArim <arim99999@gmail.com>
2024-06-02 13:15:00 +0600

    categories set

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get("/categories", (req, res) => {
 app.get("/news", (req, res) => {
   res.send(news);
 });
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((c) => parseInt(c.category_id) === id);
+    res.send(categoryNews);
+  }
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const seletedNews = news.find((n) => n._id === id);
+  res.send(seletedNews);
+});
 app.listen(port, () => {
   console.log(`news is running ${port}`);
 });
